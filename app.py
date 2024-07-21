1b723b8f6b55958ba6556ccef6dc0cc1acdb9032
Nguyen Hung <nguyenhung.gl.2212@gmail.com>
2024-07-22 00:24:17 +0700

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template,redirect,url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def default():
-    return redirect(url_for('index'))
+def index():
+    return render_template('index.html')
 
 
 @app.route('/trang-chu')
