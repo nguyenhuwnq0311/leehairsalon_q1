5bc621b56e5b68a4f14794940810f7924ed53283
Nguyen Hung <nguyenhung.gl.2212@gmail.com>
2024-07-22 22:28:46 +0700

    update

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import Flask, render_template,redirect,url_for
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def default():
-    return redirect(url_for('index'))
-
+    return redirect(url_for('home'))
 
 @app.route('/trang-chu')
 def home():
     return render_template('home.html')
+
 @app.route('/ve-chung-toi')
 def about():
     return render_template('about.html')
+
 @app.route('/dat-hen')
 def schedule():
     return render_template('schedule.html')
+
 @app.route('/chinh-sach-bao-hanh')
 def policy():
     return render_template('policy.html')
+
 @app.route('/tin-tuc')
 def news():
     return render_template('news.html')
+
 @app.route('/bang-gia-dich-vu')
 def price():
     return render_template('price.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True,port=80)
+    app.run(debug=True)
